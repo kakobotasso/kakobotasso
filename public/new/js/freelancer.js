cdf6ac16fcdc830830e2173e0322524e93b1cdc3
Kako Botasso <kakodev@gmail.com>
2017-04-08 21:34:14 -0300

    Adiciona ano ao rodapé

--- public/new/js/freelancer.js
+++ public/new/js/freelancer.js
@@ -63,5 +63,12 @@
       }
     });
 
+    // MOSTRA ANO ATUAL
+    var mydate = new Date();
+    var year = mydate.getYear();
+    if (year < 1000){
+        year+=1900
+    }
+    $("#ano").text(year)
 
 })(jQuery); // End of use strict
