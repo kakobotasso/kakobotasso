1a86c2598cb336d211b4a86617c1c5347bbaa1ac
Kako Botasso <kakodev@gmail.com>
2017-04-09 00:44:10 -0300

    Carrega lista de apps

--- public/new/js/firebase.js
+++ public/new/js/firebase.js
@@ -8,7 +8,9 @@ var config = {
 };
 firebase.initializeApp(config);
 
-// CARREGA ARTIGOS
+/* ********************************************************
+***** CARREGA ARTIGOS
+******************************************************** */
 firebase.database().ref('artigos/').once('value').then(function(snapshot) {
   var artigos = [];
   var objeto = snapshot.val()
@@ -18,7 +20,9 @@ firebase.database().ref('artigos/').once('value').then(function(snapshot) {
   document.getElementById("guardaArtigos").innerHTML = artigos.reverse().join('');
 });
 
-// CARREGA TEXTOS
+/* ********************************************************
+***** CARREGA TEXTOS
+******************************************************** */
 firebase.database().ref('textos/').once('value').then(function(snapshot) {
   var objeto = snapshot.val()
 
@@ -30,7 +34,9 @@ firebase.database().ref('textos/').once('value').then(function(snapshot) {
   }
 });
 
-// MOSTRA ANO ATUAL
+/* ********************************************************
+***** MOSTRA ANO ATUAL
+******************************************************** */
 var mydate = new Date();
 var year = mydate.getYear();
 if (year < 1000){
@@ -39,7 +45,9 @@ if (year < 1000){
 document.getElementById("ano").innerHTML = year;
 
 
-// DOWNLOAD CV
+/* ********************************************************
+***** DOWNLOAD CV
+******************************************************** */
 var storage = firebase.storage();
 var storageRef = storage.ref();
 
@@ -54,3 +62,86 @@ var cvEn = storage.refFromURL('gs://kako-botasso.appspot.com/resume-kako-botasso
 cvEn.getDownloadURL().then(function(url) {
   document.getElementById("cvEN").setAttribute("href", url);
 }).catch(function(error) {});
+
+/* ********************************************************
+***** LISTAGEM DE TRABALHOS
+******************************************************** */
+
+function itemPortifolio(titulo, index){
+  var _html = ""
+  _html += "<div class='col-sm-4 portfolio-item'>";
+  _html += "    <a href='#portfolioModal"+ index +"' class='portfolio-link' data-toggle='modal'>";
+  _html += "        <div class='caption'>";
+  _html += "            <div class='caption-content'>";
+  _html += "                <i class='fa fa-search-plus fa-3x'></i>";
+  _html += "            </div>";
+  _html += "        </div>";
+  _html += "        <img src='img/portfolio/cabin.png' class='img-responsive' alt='"+ titulo +"'>";
+  _html += "    </a>";
+  _html += "</div>";
+
+  return _html;
+}
+
+function itemModal(index, titulo, descricao, cliente, data, servico){
+  var _html = "";
+
+  _html += "<div class='portfolio-modal modal fade' id='portfolioModal"+ index +"' tabindex='-1' role='dialog' aria-hidden='true'>"
+  _html += "    <div class='modal-content'>"
+  _html += "        <div class='close-modal' data-dismiss='modal'>"
+  _html += "            <div class='lr'>"
+  _html += "                <div class='rl'>"
+  _html += "                </div>"
+  _html += "            </div>"
+  _html += "        </div>"
+  _html += "        <div class='container'>"
+  _html += "            <div class='row'>"
+  _html += "                <div class='col-lg-8 col-lg-offset-2'>"
+  _html += "                    <div class='modal-body'>"
+  _html += "                        <h2>"+ titulo +"</h2>"
+  _html += "                        <hr class='star-primary'>"
+  _html += "                        <img src='img/portfolio/cabin.png' class='img-responsive img-centered' alt=''>"
+  _html += "                        <p>"+ descricao +"</p>"
+  _html += "                        <ul class='list-inline item-details'>"
+  _html += "                            <li>Client:"
+  _html += "                                <strong><a href='http://startbootstrap.com'>"+ cliente +"</a></strong>"
+  _html += "                            </li>"
+  _html += "                            <li>Date:"
+  _html += "                                <strong><a href='http://startbootstrap.com'>"+ data +"</a></strong>"
+  _html += "                            </li>"
+  _html += "                            <li>Service:"
+  _html += "                                <strong><a href='http://startbootstrap.com'>"+ servico +"</a></strong>"
+  _html += "                            </li>"
+  _html += "                        </ul>"
+  _html += "                        <button type='button' class='btn btn-default' data-dismiss='modal'><i class='fa fa-times'></i> Close</button>"
+  _html += "                    </div>"
+  _html += "                </div>"
+  _html += "            </div>"
+  _html += "        </div>"
+  _html += "    </div>"
+  _html += "</div>"
+
+  return _html;
+}
+
+var index = 0;
+var modals = [];
+
+firebase.database().ref('apps/').once('value').then(function(snapshot) {
+  var apps = [];
+  var objeto = snapshot.val()
+
+  for(var key in objeto){
+    var cliente = objeto[key]["cliente"];
+    var data = objeto[key]["data"];
+    var descricao = objeto[key]["descricao"];
+    var servico = objeto[key]["servico"];
+    var titulo = objeto[key]["titulo"];
+
+    apps.push( itemPortifolio(titulo, index) );
+    modals.push( itemModal(index, titulo, descricao, cliente, data, servico) );
+    index += 1;
+  }
+  document.getElementById("guardaApps").innerHTML = apps.reverse().join('');
+  document.getElementById("guardaModals").innerHTML = modals.reverse().join('');
+});
